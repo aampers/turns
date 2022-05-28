d1caf7e57dc4f02831a77e04626c527943e0cae0
Alan Ray Ampersand <60027028+aampers@users.noreply.github.com>
2022-05-28 09:53:10 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => (
+	<header>
+		<nav className='navbar' role='navigation' aria-label='main navigation'>
+			<div className='navbar-brand'>
+				<h1>Hi mom.</h1>
+			</div>
+		</nav>
+	</header>
+);
+
+export default Header;
